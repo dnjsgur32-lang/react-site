1f1ea8a860bd06b663a153af2f7b440f58e48cca
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-08-11 15:35:43 +0900

    Updates

--- src/compo/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div>
-            <h2>홈페이지 메인입니다.</h2>
-
-        </div>
-    )
-}
-export default Home;
--- src/compo/Login.jsx
+++
@@ -1,9 +0,0 @@
-function Login() {
-    return (
-        <div>
-            <h2>로그인 홈페이지 LOGIN</h2>
-
-        </div>
-    )
-}
-export default Login;

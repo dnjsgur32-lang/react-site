f2093081e0ff6d0a28c068dbdf4e5c01781be6d1
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-08-11 15:55:42 +0900

    adit 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './compo/Layout'
 import Login from './compo/Login'
 import Home from './compo/Home'
-import { useState } from 'react'
 
 function App() {
 
@@ -13,13 +11,12 @@ function App() {
       <h1>과제방</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/react-site/" element={<Layout />} />
-          <Route index element={<Home />} />
-          <Route path="/react-site/login" element={<Login />} />
-          <Route />
+          <Route path="/react-site/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/react-site/login" element={<Login />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-      {/* dasdsadasd */}
     </>
   )
 }

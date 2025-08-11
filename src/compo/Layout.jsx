23051b196309c9e28ab20b2c98b42a567b6ba931
dnjsgur32-lang <dnjsgur32@gmail.com>
2025-08-11 14:54:39 +0900

    Initial commit

---
+++ src/compo/Layout.jsx
@@ -0,0 +1,23 @@
+import {Link, Outlet} from "react-router-dom"
+
+function Layout() {
+
+    return (
+        <div>
+            <h2>홈페이지</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/react-site">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/react-site/login">Login</Link>
+                    </li>
+                    
+                </ul>
+            </nav>
+            <Outlet/>
+        </div>
+    )
+}
+export default Layout;
---
+++ src/compo/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <div>
+            <h2>로그인 홈페이지 LOGIN</h2>
+
+        </div>
+    )
+}
+export default Login;
